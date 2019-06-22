871ba48ec5d554c69d7d41898d3fc47407542947
mydearxym <mydearxym@qq.com>
2019-06-22 12:12:42 +0800

    chore: setup

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-native', 'eslint-plugin-react-hooks'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+      },
+    },
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'max-len': ['error', 120],
+    quotes: [2, 'single', { avoidEscape: true }],
+    'no-use-before-define': ['error', { variables: false }],
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-color-literals': 0,
+    'react-native/no-raw-text': 0,
+    'import/no-extraneous-dependencies': 2,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2,
+        bracketSpacing: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
